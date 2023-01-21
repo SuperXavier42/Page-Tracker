a29e51c6de66bcf6e7e88659316c5f24388b00ee
Xavier Le <superxavier100@gmail.com>
2023-01-20 21:09:15 -0600

    login and sign up working

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ import sqlite3
 
 from flask import g
 
-db_file = 'C:\Program Files\SQLiteStudio\data.db'
+db_file = 'D:\VSCode\Website\data.db'
 
 def get_db():
     connection = g.get('db', 'null')
--- users.py
+++ users.py
@@ -4,42 +4,29 @@ from passlib.hash import pbkdf2_sha256 as pw
 def create_table():
     connection = get_db()
     sql = connection.cursor()
-    sql.execute(
-        """create table if not exists users (
+    sql.execute("""create table if not exists users (
         "id" integer primary key autoincrement,
         "username" Text,
-        "password" Text
-    ) """
-    )
+        "password" Text) """)
 
 
 def create_account(username, password):
     connection = get_db()
     sql = connection.cursor()
-    result = sql.execute("select * from users where username = ?", [username])
-    rows = result.fetchall()
-    if len(rows) > 0:
-        return "Username already exsists"
-    else:
-        hashed = pw.hash(password)
-        sql.execute(
-            """insert into users 
-        (username, password) values (?, ?)""",
-            [username, hashed],
-        )
-        connection.commit()
-        return "Account created"
+    hashed = pw.hash(password)
+    sql.execute("insert into users (username, password) values (?, ?)",[username, hashed])   
+    connection.commit()
+    return "Account created"
 
 def check_account(username, password):
     connection = get_db()
     sql = connection.cursor()
-    result = sql.execute('''select * from users
-                            where username = ?''', [username])
+    result = sql.execute('''select * from users where username = ?''', [username])
     data = result.fetchone()
     if data:
         hashed = data['password']
         check_hash = pw.verify(password, hashed)
         if check_hash:
-            return data['userId']
+            return data['id']
     else:
         return False
