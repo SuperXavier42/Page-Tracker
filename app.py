e89a35a4455a7e36e4c0b9ab05993bea6287b10e
Xavier Le <superxavier100@gmail.com>
2023-04-02 16:29:34 -0500

    Using MAL database

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 
+import requests
+import secrets
 from flask import Flask, render_template, redirect, request, session, url_for
 from .users import create_table, check_account, create_account
 from .auth import auth_bp
+
+def get_new_code_verifier() -> str:
+    token = secrets.token_urlsafe(100)
+    return token[:128]
+
+code_verifier = code_challenge = get_new_code_verifier()
 
 app = Flask(__name__)
 
 app.secret_key = "gUG*7BNmM*[*hUd7&y6hb}GlTcub`C"
 
 app.register_blueprint(auth_bp)
+
+url="https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=79a3722f91d0b23adeadcd21f4b5896e&code_challenge="+str(code_challenge)
+
+MAL = requests.get(url)
 
 with app.app_context():
     create_table()
