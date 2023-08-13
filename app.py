88a64603a28a33cfaedf1713cfbb2321a2da1f58
Xavier Le <superxavier100@gmail.com>
2023-08-12 20:03:06 -0500

    i changed the website

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 
-import requests
+
 import secrets
 from flask import Flask, render_template, redirect, request, session, url_for
 from .users import create_table, check_account, create_account
 from .auth import auth_bp
-
-def get_new_code_verifier() -> str:
-    token = secrets.token_urlsafe(100)
-    return token[:128]
-
-code_verifier = code_challenge = get_new_code_verifier()
 
 app = Flask(__name__)
 
@@ -18,9 +12,7 @@ app.secret_key = "gUG*7BNmM*[*hUd7&y6hb}GlTcub`C"
 
 app.register_blueprint(auth_bp)
 
-url="https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=79a3722f91d0b23adeadcd21f4b5896e&code_challenge="+str(code_challenge)
 
-MAL = requests.get(url)
 
 with app.app_context():
     create_table()
@@ -33,7 +25,11 @@ def home():
     else:
         return redirect(url_for('auth.login_page'))
         
-@app.route('/watch')
-def watch():
-    return render_template('watch.html')        
+@app.route('/track', methods=['GET', 'POST'])
+def track():
+    if request.method == "POST":
+        name=request.form['book-name']
+        pages=request.form['num-pages']
+        days=request.form['days']
+    return render_template('track.html')        
     
