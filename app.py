df16b01d02febbc67bd607e8503ac08da02e759c
Xavier Le <superxavier100@gmail.com>
2023-08-15 20:32:42 -0500

    fixed imports

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-import secrets
 from flask import Flask, render_template, redirect, request, session, url_for
 from users import create_table, check_account, create_account
 from auth import auth_bp
-from connection import create_app
 
-app = create_app()
+app = Flask(__name__)
+
 app.secret_key = "gUG*7BNmM*[*hUd7&y6hb}GlTcub`C"
+
+app.register_blueprint(auth_bp)
+
+
 
 with app.app_context():
     create_table()
-
-app.register_blueprint(auth_bp)
 
 @app.route('/')
 def home():
--- connection.py
+++ connection.py
@@ -1,20 +1,7 @@
 import sqlite3
-from flask import g
-from urllib import parse
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-
-db = SQLAlchemy()
 
 def get_db():
     con = sqlite3.connect('data.db', check_same_thread=False)
     con.row_factory = sqlite3.Row
     return con
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-    db.init_app(app)
-    with app.app_context():
-       db.create_all()
-    return app
