6119c015691788ef0f18dbdd48d3b71eacf47a0c
Xavier <superxavier100@gmail.com>
2024-09-25 14:15:06 -0500

    Creates new book data base cleaned up overall routes

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, redirect, request, session, url_for
-from users import create_table, check_account, create_account
+from users import create_table
+from book_data import create_books
 from auth import auth_bp
+from books import book_bp
 
 app = Flask(__name__)
 
 app.secret_key = "gUG*7BNmM*[*hUd7&y6hb}GlTcub`C"
 
 app.register_blueprint(auth_bp)
+app.register_blueprint(book_bp)
 
 
 
 with app.app_context():
     create_table()
+    create_books()
 
 @app.route('/')
 def home():
@@ -21,11 +25,3 @@ def home():
     else:
         return redirect(url_for('auth.login_page'))
         
-@app.route('/track', methods=['GET', 'POST'])
-def track():
-    if request.method == "POST":
-        name=request.form['book-name']
-        pages=request.form['num-pages']
-        days=request.form['days']
-    return render_template('track.html')        
-    
