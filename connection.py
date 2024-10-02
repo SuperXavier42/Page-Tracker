44cace23c755aee9399e1d45a597e440033104e2
Xavier <superxavier100@gmail.com>
2024-10-02 11:44:37 -0500

    added a seperate get_db_tuple for accessing books

--- connection.py
+++ connection.py
@@ -5,3 +5,7 @@ def get_db():
     con = sqlite3.connect('data.db', check_same_thread=False)
     con.row_factory = sqlite3.Row
     return con
+
+def get_db_tuple():
+    con = sqlite3.connect('data.db', check_same_thread=False)
+    return con
