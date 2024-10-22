a426c4ef1123cf2f63ff7b6d5a9722b8d2c84d83
Xavier <superxavier100@gmail.com>
2024-10-22 10:02:33 -0500

    created route for deleting books, reading books and planning books

--- books.py
+++ books.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, session, request, url_for
-from book_data import add_book, locate_books
+from book_data import add_book, locate_books, delete_book
 
 book_bp = Blueprint('book', __name__)
 
@@ -7,9 +7,24 @@ book_bp = Blueprint('book', __name__)
 def addbook():
     notif=""
     if request.method == "POST":
-        name=request.form['book-name']
-        pages=request.form['num-pages']
+        name=request.form['book_name']
+        pages=request.form['num_pages']
         days=request.form['days']
         notif=add_book(name, pages, days)
     return render_template('addbook.html', error=notif)        
     
+@book_bp.route('/deletebook', methods=['GET', 'POST'])
+def deletebook():
+    if request.method == "POST":
+        to_delete=request.form['book_name']
+        delete_book(to_delete)
+    return redirect(url_for('book.current_books'))
+
+@book_bp.route('/reading', methods=['GET', 'POST'])
+def current_books():
+    books = locate_books()
+    return render_template('reading.html', books=books)
+
+@book_bp.route('/planning', methods=['GET', 'POST'])
+def future_books():
+    return redirect(url_for('home'))
