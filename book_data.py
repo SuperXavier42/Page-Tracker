804862f2507c612f309f59dfb72cd9be7ff1e008
Xavier <superxavier100@gmail.com>
2024-10-23 18:48:39 -0500

    check inputs

--- book_data.py
+++ book_data.py
@@ -24,12 +24,15 @@ def add_book(book_name, page_total, days_left):
     if(len(rows)>0):
         return "Book already being tracked"
     else:
-        date=datetime.now()
-        target_date=date + timedelta(days=int(days_left))
-        target_date = target_date.replace(hour=0, minute=0, second=0)
-        sql.execute("INSERT into books (user_id, book_name, page_total, days_left, target_date) VALUES (?, ?, ?, ?, ?)", [user_id, book_name, page_total, days_left, target_date])
-        connection.commit()
-        return "Tracking book"
+        if(int(days_left)>0 and int(page_total)>0):
+            date=datetime.now()
+            target_date=date + timedelta(days=int(days_left)+1)
+            target_date = target_date.replace(hour=0, minute=0, second=0)
+            sql.execute("INSERT into books (user_id, book_name, page_total, days_left, target_date) VALUES (?, ?, ?, ?, ?)", [user_id, book_name, page_total, days_left, target_date])
+            connection.commit()
+            return "Tracking book"
+        else:
+            return "Invalid Input"
     
 def delete_book(book_name):
     connection = get_db_tuple()
@@ -45,7 +48,10 @@ def complete_book(book_name, num_pages):
     user_id = session.get('user_id')
     total_pages = sql.execute("SELECT page_total FROM books WHERE (user_id = ? AND book_name = ?)", [user_id, book_name]).fetchone()[0]
     pages_left = total_pages - int(num_pages)
-    sql.execute("UPDATE books SET page_total = ? WHERE (user_id = ? AND book_name = ?)", [round(pages_left), user_id, book_name])
+    if(pages_left <= 0):
+        sql.execute("DELETE FROM books WHERE (user_id = ? AND book_name = ?)", [user_id, book_name])
+    else:
+        sql.execute("UPDATE books SET page_total = ? WHERE (user_id = ? AND book_name = ?)", [round(pages_left), user_id, book_name])
     connection.commit()
     return "Book completed"
 
@@ -64,13 +70,13 @@ def days_remaining(book_name):
     connection = get_db_tuple()
     sql = connection.cursor()
     user_id = session.get('user_id')
-    result = sql.execute("SELECT target_date FROM books WHERE (user_id = ? AND book_name = ?)", [user_id, book_name])
+    result = sql.execute("SELECT target_date, page_total FROM books WHERE (user_id = ? AND book_name = ?)", [user_id, book_name])
     row=result.fetchone()
     target_date = datetime.strptime(row[0][0:19], '%Y-%m-%d %H:%M:%S')
     days_left=target_date - datetime.now()
-    if(days_left<0):
+    if(days_left.days<0):
         sql.execute("DELETE FROM books WHERE (user_id = ? AND book_name = ?)", [user_id, book_name])
-        sql.execute("INSERT into planned_books (user_id, book_name) VALUES (?, ?)", [user_id, book_name])
+        sql.execute("INSERT into planned_books (user_id, book_name, page_count) VALUES (?, ?, ?)", [user_id, book_name, row[1]])
     else:
         sql.execute("UPDATE books SET days_left = ? WHERE (user_id = ? AND book_name = ?)", [int(days_left.days), user_id, book_name])
     connection.commit()
